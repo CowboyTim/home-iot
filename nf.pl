0c8ca17e356c1123b863e4c314fc01718cc8bc61
Tim Aerts <aardbeiplantje@gmail.com>
2025-02-15 22:43:12 +0100

    nf.pl: improve logging

--- nf.pl
+++ nf.pl
@@ -123,7 +123,7 @@ our $NF_REPEAT = 4;
 
 our $SOL_NETLINK = 270;
 
-my $queue_num = shift @ARGV // die "Usage: $0 QUEUE_NUM\n";
+my $queue_num = shift @ARGV // die "Usage: $0 QUEUE_NUM";
 my $my_id = 0;#$$;
 
 socket(my $nf_fh, $AF_NETLINK, SOCK_RAW, $NETLINK_NETFILTER)
@@ -133,9 +133,9 @@ bind($nf_fh, $bind_addr)
     // die "bind: $!";
 binmode($nf_fh);
 my $s_flags = fcntl($nf_fh, F_GETFL, 0)
-    or die "Can't get flags for the socket: $!\n";
+    or die "Can't get flags for the socket: $!";
 fcntl($nf_fh, F_SETFL, $s_flags|O_NONBLOCK)
-    or die "Can't set flags for the socket: $!\n";
+    or die "Can't set flags for the socket: $!";
 setsockopt($nf_fh, $SOL_NETLINK, $NETLINK_EXT_ACK, 0)
     or die "setsockopt: $!";
 setsockopt($nf_fh, $SOL_NETLINK, $NETLINK_CAP_ACK, 0)
@@ -149,7 +149,7 @@ nfqnl_send($nf_fh, $bind_addr,
     nfqnl_copy_packet($queue_num, 0xffff) # full size copy
 ) or die "nfqnl_send: $!";
 
-print "Listening for packets on queue $queue_num\n";
+log_info("Listening for packets on queue $queue_num");
 M_LOOP:
 while(1){
     local $!;
@@ -165,12 +165,12 @@ while(1){
         my $r = recv($nf_fh, my $pkt_msg, 1_000_000, 0);
         if(!defined $r){
             next M_LOOP if $!{EAGAIN};
-            die "recv problem for: $!\n";
+            die "recv problem for: $!";
         }
         my ($m_seq, $m_pid, $pktids) = handle_nlmsg($pkt_msg);
         # send positive verdict
         foreach my $pkt_id (@$pktids){
-            print "Sending verdict for packet $pkt_id\n";
+            log_info("Sending verdict for packet $pkt_id");
             nfqnl_send($nf_fh, $bind_addr, nfqnl_msg_verdict($m_seq, $m_pid, $queue_num, $pkt_id, $NF_ACCEPT))
                 or die "nfqnl_send: $!";
         }
@@ -258,20 +258,20 @@ sub nlmsghdr {
 sub handle_nlmsg {
     my ($msg) = @_;
     my ($len, $type, $flags, $seq, $pid, $data) = unpack("LSSLLa*", $msg);
-    print "MSG[".length($msg)."]: len: $len, type: $type, flags: $flags, seq: $seq, pid: $pid\n";
+    log_debug("MSG[".length($msg)."]: len: $len, type: $type, flags: $flags, seq: $seq, pid: $pid");
     if($type == $NLMSG_ERROR){
         my $err_num = unpack("La*", $data);
-        die "ERROR[$err_num]\n";
+        die "ERROR[$err_num]";
     }
     if($flags & $NLM_F_ACK){
-        print "ACK\n";
+        log_debug("ACK");
         return;
     }
     my $ret;
     if($type == ($NFNL_SUBSYS_QUEUE<<8|$NFQNL_MSG_PACKET)){
         $ret = handle_nfqnl_msg_packet($data);
     } else {
-        print "ERROR: Unknown message type: $type\n";
+        log_debug("ERROR: Unknown message type: $type");
     }
     return $seq, $pid, $ret;
 }
@@ -279,117 +279,151 @@ sub handle_nlmsg {
 sub handle_nfqnl_msg_packet {
     my ($data) = @_;
     my ($gen_hdr_nf_family, $gen_hdr_nf_version, $gen_hdr_nf_res_id, $nl_attrs) = unpack("CCS>a*", $data);
-    print "gen_hdr_nf_family: $gen_hdr_nf_family\n";
-    print "gen_hdr_nf_version: $gen_hdr_nf_version\n";
-    print "queue_number $gen_hdr_nf_res_id\n";
+    log_debug("gen_hdr_nf_family: $gen_hdr_nf_family");
+    log_debug("gen_hdr_nf_version: $gen_hdr_nf_version");
+    log_debug("queue_number $gen_hdr_nf_res_id");
     my @pkt_ids;
     while(length($nl_attrs) > 0){
-        print " hex: ".to_hex($nl_attrs)."\n";
+        log_debug(" hex: ".to_hex($nl_attrs)."");
         my ($nla_len, $nla_type) = unpack("SS", $nl_attrs);
         my $nr_pad = $nla_len % 4 ?4 - $nla_len % 4: 0;
         my $nla_data = substr($nl_attrs, 0, $nla_len, '');
         substr($nla_data, 0, 4, '');
-        print " nr_pad: $nr_pad\n";
-        print " nla_type: $nla_type\n";
-        print " nla_len: $nla_len\n";
-        print " nla_data: ".to_hex($nla_data)."\n";
-        print " remaining: ".length($nl_attrs)."\n";
+        log_debug(" nr_pad: $nr_pad");
+        log_debug(" nla_type: $nla_type");
+        log_debug(" nla_len: $nla_len");
+        log_debug(" nla_data: ".to_hex($nla_data)."");
+        log_debug(" remaining: ".length($nl_attrs)."");
         if($nla_type == $NFQA_PACKET_HDR){
             my ($packet_id, $hw_protocol, $hook) = unpack("L>S>C", $nla_data);
-            print " - packet_id: $packet_id\n";
-            print " - hw_protocol: $hw_protocol\n";
-            print " - hook: $hook\n";
+            log_debug(" - packet_id: $packet_id");
+            log_debug(" - hw_protocol: $hw_protocol");
+            log_debug(" - hook: $hook");
             push @pkt_ids, $packet_id;
         } elsif($nla_type == $NFQA_HWADDR){
             my ($hw_addrlen, $pad) = unpack("S>S", $nla_data);
             my $hw_addr = substr($nla_data, 0, $hw_addrlen, '');
-            print " - hw_addrlen: $hw_addrlen\n";
-            print " - hw_addr: ".to_hex($hw_addr)."\n";
+            log_debug(" - hw_addrlen: $hw_addrlen");
+            log_debug(" - hw_addr: ".to_hex($hw_addr)."");
         } elsif($nla_type == $NFQA_TIMESTAMP){
             my ($sec, $usec) = unpack("Q>Q>", $nla_data);
-            print " - sec: $sec\n";
-            print " - usec: $usec\n";
+            log_debug(" - sec: $sec");
+            log_debug(" - usec: $usec");
         } elsif($nla_type == $NFQA_IFINDEX_INDEV){
             my $ifindex = unpack("L>", $nla_data);
-            print " - ifindex: $ifindex\n";
+            log_debug(" - ifindex: $ifindex");
         } elsif($nla_type == $NFQA_IFINDEX_OUTDEV){
             my $ifindex = unpack("L>", $nla_data);
-            print " - ifindex: $ifindex\n";
+            log_debug(" - ifindex: $ifindex");
         } elsif($nla_type == $NFQA_IFINDEX_PHYSINDEV){
             my $ifindex = unpack("L>", $nla_data);
-            print " - ifindex: $ifindex\n";
+            log_debug(" - ifindex: $ifindex");
         } elsif($nla_type == $NFQA_IFINDEX_PHYSOUTDEV){
             my $ifindex = unpack("L>", $nla_data);
-            print " - ifindex: $ifindex\n";
+            log_debug(" - ifindex: $ifindex");
         } elsif($nla_type == $NFQA_PAYLOAD){
-            print " - payload[hex]: ".to_hex($nla_data)."\n";
+            log_debug(" - payload[hex]: ".to_hex($nla_data)."");
             my $ip_version = unpack("C", $nla_data);
             my ($iphdr, $tcphdr, $payload);
             if(($ip_version & 0xf0) == 0x40){
                 ($iphdr, $tcphdr, $payload) = unpack("a20a32a*", $nla_data);
-                print " - iphdr[hex]: ".to_hex($iphdr)."\n";
+                log_debug(" - iphdr[hex]: ".to_hex($iphdr)."");
                 # let's parse the ipv4 header
                 my ($ip_version_ihl, $ip_dscp_ecn, $ip_tot_len, $ip_id, $ip_flags_fragment_offset, $ip_ttl, $ip_protocol, $ip_check, $ip_saddr, $ip_daddr) = unpack("CCS>S>S>CCS>a4a4", $iphdr);
                 my $ip_version = $ip_version_ihl >> 4;
                 my $ip_ihl = $ip_version_ihl & 0x0f;
-                print " - ip_version: $ip_version\n";
-                print " - ip_ihl: $ip_ihl\n";
-                print " - ip_dscp_ecn: $ip_dscp_ecn\n";
-                print " - ip_tot_len: $ip_tot_len\n";
-                print " - ip_id: $ip_id\n";
-                print " - ip_flags_fragment_offset: $ip_flags_fragment_offset\n";
-                print " - ip_ttl: $ip_ttl\n";
-                print " - ip_protocol: $ip_protocol\n";
-                print " - ip_check: $ip_check\n";
-                print " - ip_saddr: ".inet_ntoa($ip_saddr)."\n";
-                print " - ip_daddr: ".inet_ntoa($ip_daddr)."\n";
+                log_debug(" - ip_version: $ip_version");
+                log_debug(" - ip_ihl: $ip_ihl");
+                log_debug(" - ip_dscp_ecn: $ip_dscp_ecn");
+                log_debug(" - ip_tot_len: $ip_tot_len");
+                log_debug(" - ip_id: $ip_id");
+                log_debug(" - ip_flags_fragment_offset: $ip_flags_fragment_offset");
+                log_debug(" - ip_ttl: $ip_ttl");
+                log_debug(" - ip_protocol: $ip_protocol");
+                log_debug(" - ip_check: $ip_check");
+                log_debug(" - ip_saddr: ".inet_ntoa($ip_saddr)."");
+                log_debug(" - ip_daddr: ".inet_ntoa($ip_daddr)."");
             } elsif(($ip_version & 0xf0) == 0x60){
                 ($iphdr, $tcphdr, $payload) = unpack("a40a32a*", $nla_data);
-                print " - ipv6hdr[hex]: ".to_hex($iphdr)."\n";
+                log_debug(" - ipv6hdr[hex]: ".to_hex($iphdr)."");
                 # let's parse the ipv6 header
                 my ($ip6_version, $ip6_traffic_class, $ip6_flow_label, $ip6_payload_len, $ip6_next_header, $ip6_hop_limit, $ip6_src, $ip6_dst) = unpack("CCS>S>CCa16a16", $iphdr);
                 $ip6_version &= 0xff;
-                print " - ip6_version: $ip6_version\n";
-                print " - ip6_traffic_class: $ip6_traffic_class\n";
-                print " - ip6_flow_label: $ip6_flow_label\n";
-                print " - ip6_payload_len: $ip6_payload_len\n";
-                print " - ip6_next_header: $ip6_next_header\n";
-                print " - ip6_hop_limit: $ip6_hop_limit\n";
-                print " - ip6_src: ".inet_ntop(AF_INET6, $ip6_src)."\n";
-                print " - ip6_dst: ".inet_ntop(AF_INET6, $ip6_dst)."\n";
+                log_debug(" - ip6_version: $ip6_version");
+                log_debug(" - ip6_traffic_class: $ip6_traffic_class");
+                log_debug(" - ip6_flow_label: $ip6_flow_label");
+                log_debug(" - ip6_payload_len: $ip6_payload_len");
+                log_debug(" - ip6_next_header: $ip6_next_header");
+                log_debug(" - ip6_hop_limit: $ip6_hop_limit");
+                log_debug(" - ip6_src: ".inet_ntop(AF_INET6, $ip6_src)."");
+                log_debug(" - ip6_dst: ".inet_ntop(AF_INET6, $ip6_dst)."");
             } else {
-                print " - unknown ip_version: $ip_version:".($ip_version & 0xff)."\n";
+                log_debug(" - unknown ip_version: $ip_version:".($ip_version & 0xff)."");
             }
-            print " - tcphdr[hex]: ".to_hex($tcphdr)."\n";
-            print " - payload[hex]: ".to_hex($payload)."\n";
-            print " - payload[raw]: ".$payload."\n";
+            # now we parse the tcp header
+            my ($tcp_sport, $tcp_dport, $tcp_seq, $tcp_ack_seq, $tcp_data_off_res, $tcp_flags, $tcp_window, $tcp_check, $tcp_urg_ptr) = unpack("S>S>L>L>CCS>S>S>", $tcphdr);
+            my $tcp_data_off = $tcp_data_off_res >> 4;
+            my $tcp_res = $tcp_data_off_res & 0x0f;
+            log_debug(" - tcp_sport: $tcp_sport");
+            log_debug(" - tcp_dport: $tcp_dport");
+            log_debug(" - tcp_seq: $tcp_seq");
+            log_debug(" - tcp_ack_seq: $tcp_ack_seq");
+            log_debug(" - tcp_data_off: $tcp_data_off");
+            log_debug(" - tcp_res: $tcp_res");
+            log_debug(" - tcp_flags: $tcp_flags");
+            # and parse the flags
+            my $tcp_fin = $tcp_flags & 0x01;
+            my $tcp_syn = ($tcp_flags & 0x02) >> 1;
+            my $tcp_rst = ($tcp_flags & 0x04) >> 2;
+            my $tcp_psh = ($tcp_flags & 0x08) >> 3;
+            my $tcp_ack = ($tcp_flags & 0x10) >> 4;
+            my $tcp_urg = ($tcp_flags & 0x20) >> 5;
+            my $tcp_ece = ($tcp_flags & 0x40) >> 6;
+            my $tcp_cwr = ($tcp_flags & 0x80) >> 7;
+            log_debug(" - tcp_fin: $tcp_fin");
+            log_debug(" - tcp_syn: $tcp_syn");
+            log_debug(" - tcp_rst: $tcp_rst");
+            log_debug(" - tcp_psh: $tcp_psh");
+            log_debug(" - tcp_ack: $tcp_ack");
+            log_debug(" - tcp_urg: $tcp_urg");
+            log_debug(" - tcp_ece: $tcp_ece");
+            log_debug(" - tcp_cwr: $tcp_cwr");
+            log_debug(" - tcp_window: $tcp_window");
+            log_debug(" - tcp_check: $tcp_check");
+            log_debug(" - tcp_urg_ptr: $tcp_urg_ptr");
+            log_debug(" - tcphdr[hex]: ".to_hex($tcphdr)."");
+            log_debug(" - payload[hex]: ".to_hex($payload)."");
+            log_debug(" - payload[raw]: ".$payload."");
+            if($tcp_psh){
+                print $payload;
+            }
         } elsif($nla_type == $NFQA_CT){
-            print " - conntrack[hex]: ".to_hex($nla_data)."\n";
+            log_debug(" - conntrack[hex]: ".to_hex($nla_data)."");
         } elsif($nla_type == $NFQA_CT_INFO){
-            print " - conntrack_info[hex]: ".to_hex($nla_data)."\n";
+            log_debug(" - conntrack_info[hex]: ".to_hex($nla_data)."");
         } elsif($nla_type == $NFQA_MARK){
             my $mark = unpack("L>", $nla_data);
-            print " - mark: $mark\n";
+            log_debug(" - mark: $mark");
         } elsif($nla_type == $NFQA_VLAN){
             my ($vlan_tci, $vlan_proto) = unpack("S>S>", $nla_data);
-            print " - vlan_tci: $vlan_tci\n";
-            print " - vlan_proto: $vlan_proto\n";
+            log_debug(" - vlan_tci: $vlan_tci");
+            log_debug(" - vlan_proto: $vlan_proto");
         } elsif($nla_type == $NFQA_L2HDR){
-            print " - l2hdr[hex]: ".to_hex($nla_data)."\n";
+            log_debug(" - l2hdr[hex]: ".to_hex($nla_data)."");
         } elsif($nla_type == $NFQA_EXP){
-            print " - exp[hex]: ".to_hex($nla_data)."\n";
+            log_debug(" - exp[hex]: ".to_hex($nla_data)."");
         } elsif($nla_type == $NFQA_UID){
             my $uid = unpack("L>", $nla_data);
-            print " - uid: $uid\n";
+            log_debug(" - uid: $uid");
         } elsif($nla_type == $NFQA_GID){
             my $gid = unpack("L>", $nla_data);
-            print " - gid: $gid\n";
+            log_debug(" - gid: $gid");
         } elsif($nla_type == $NFQA_SECCTX){
-            print " - secctx[hex]: ".to_hex($nla_data)."\n";
+            log_debug(" - secctx[hex]: ".to_hex($nla_data)."");
         } elsif($nla_type == $NFQA_SKB_INFO){
-            print " - skb_info[hex]: ".to_hex($nla_data)."\n";
+            log_debug(" - skb_info[hex]: ".to_hex($nla_data)."");
         } else {
-            print " - unknown $nla_type\n";
+            log_debug(" - unknown $nla_type");
         }
         substr($nl_attrs, 0, $nr_pad, '');
     }
@@ -399,4 +433,16 @@ sub handle_nfqnl_msg_packet {
 sub to_hex {
     my $b = \$_[0];
     return join("",map {sprintf("%02x", ord $_)} split '', $$b//"");
+}
+
+sub log_info {
+    my ($msg) = @_;
+    print STDERR "INFO: $msg\n";
+    return;
+}
+
+sub log_debug {
+    my ($msg) = @_;
+    print STDERR "DEBUG: $msg\n" if $ENV{DEBUG};
+    return;
 }
